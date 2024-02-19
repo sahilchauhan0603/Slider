9f182489f453f6d32fb961369cee83d8b3d653f2
Sahil Chauhan <135093769+sahilchauhan0603@users.noreply.github.com>
2024-02-19 22:15:22 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Virat Kohli",
+    job: "Right-Hand Batsman",
+    // image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+    image: "https://media.gettyimages.com/id/1434940923/photo/brisbane-australia-virat-kohli-poses-during-the-india-icc-mens-t20-cricket-world-cup-2022.jpg?s=612x612&w=0&k=20&c=c3uqmkPPYpmzDtLDKcQdjgOwPjKrcCPexvAeOIAR8sg=",
+    text: "Virat Kohli is an Indian international cricketer.He is widely regarded as one of the greatest batsmen in the history of cricket.Kohli holds the record for scoring most centuries in ODIs and stands second in the list of most international centuries scored.",
+  },
+  {
+    id: 2,
+    name: "MS.Dhoni",
+    job: "Right-Hand Wicket-keeper Batsman",
+    // image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+    image: "https://media.gettyimages.com/id/171229788/photo/birmingham-england-ms-dhoni-captain-of-india-celebrates-his-teams-win-over-england-during-the.jpg?s=612x612&w=0&k=20&c=yfiQOY6YqOdeGxx1Wg0xptVRb8FfE1hA2038G5otMZY=",
+    text: "MS Dhoni is a former Indian cricketer and captain.He is popularly known as Captain Cool. He led India to win the ICC World Cup in 2011, the ICC T20 World Cup in 2007, the ICC Champions Trophy in 2013, and the Asia Cup in 2010, 2016, and 2018.",
+  },
+  {
+    id: 3,
+    name: "Rohit Sharma",
+    job: "Right Hand Batsman(Opener)",
+    // image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+    image: "https://media.gettyimages.com/id/1338270799/photo/london-england-rohit-sharma-of-india-hits-a-six-to-reach-his-century-during-day-three-of-the.jpg?s=612x612&w=0&k=20&c=l9Ngo9NYY0q0puQe16Cjn3c_bbyFsrrxj3CDERu8XwA=",
+    text: "Rohit Sharma is an Indian international cricketer and the current captain of the Indian national cricket team across all formats.He holds several batting records, such as the most double centuries in ODI cricket(3),the highest individual score in ODI cricket(264).",
+  },
+  {
+    id: 4,
+    name: "Shikhar Dhawan",
+    job: "Left Hand Batsman",
+    // image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+    image: "https://media.gettyimages.com/id/1408698382/photo/london-england-shikhar-dhawan-of-india-looks-on-during-the-2nd-royal-london-series-one-day.jpg?s=612x612&w=0&k=20&c=WwYKyIgd-NSsbNnAjcWI2uHFTYnnUVRUSz7-9ChkAyo=",
+    text: "Shikhar Dhawan plays as a left-handed opening batsman. He holds the record for the fastest Test century by a debutant.He was the leading run-scorer and the player of the tournament in the 2013 and 2017 ICC Champions Trophy and the 2018 Asia Cup.",
+  },
+  {
+    id: 5,
+    name: "Jasprit Bumrah",
+    job: "Right Arm Fast Bowler",
+    // image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+    image: "https://media.gettyimages.com/id/1406353979/photo/birmingham-england-india-bowler-jasprit-bumrah-celebrates-after-taking-the-wicket-of-zak.jpg?s=612x612&w=0&k=20&c=FCRnFZf4j0wqGBKw2XtNne3zhxErgvBONvyyPFvK-8g=",
+    text: "Jasprit Bumrah plays for the national team in all formats of the game. He is a specialist in yorkers and death overs. He is widely regarded as one of the best bowlers in the world and one of the greatest fast bowlers India has produced",
+  },
+];
+
+export default reviews;
